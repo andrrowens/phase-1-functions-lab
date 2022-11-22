dfb41e433c1e7f314de97a5586fa2ae452cba7d2
Andrew Owens <andrrowens@gmail.com>
2022-11-21 17:22:27 -0700

    commit tree 43598b09a8ee54fdcc79bf506b6fce6252f34a99

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+   const hq = 42  
+   return location - hq
+};
+
+const result = distanceFromHqInBlocks(50)
+console.log(result);
+
+function distanceFromHqInFeet(){
+    const blockLengthInFeet = 264
+    console.log(result)
+    console.log(blockLengthInFeet)
+    return result * blockLengthInFeet
+};
+
+function distanceTravelledInFeet(start, destination){
+    const blockLengthInFeet = 264
+    const blockDistance = destination - start
+    console.log(blockDistance)
+    return blockDistance * blockLengthInFeet
+};
+
+distanceTravelledInFeet(43, 48);
+distanceTravelledInFeet(50, 60);
+distanceTravelledInFeet(34, 28);
